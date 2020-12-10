244f36346d0d97b64a7c61a315531a7cfbaa0b57
Owlspec3086 <ethical.worker3@gmail.com>
2020-12-10 18:53:58 -0500

    working on project

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+const server = require('./server.js')
+
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+    console.log(`\n Access Granted on port ${port} \n`);
+});
